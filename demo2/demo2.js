1526b73ee40b98a1e108902844ea1ab5969c9f43
Hadi Ahmed <hadiahmed098@gmail.com>
2021-02-19 20:37:42 -0500

    Lesson2 Demo2 done

---
+++ demo2/demo2.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: "Testing some v- directives:",
+        imageSRC: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+        canYouSeeMe: true,
+        inputMessage: "Start",
+    }
+});
+
+// https://purdueieee.org/software/images/logo_white.jpg

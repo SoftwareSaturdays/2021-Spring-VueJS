607cce89df63f4033df9f1f36d51369d45ed1bc0
Hadi Ahmed <hadiahmed098@gmail.com>
2021-02-24 16:09:11 -0500

    Finished L4 Demo1

---
+++ demo1/demo1.js
@@ -0,0 +1,10 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        list: [
+            {id:0, txt:"Hello there"},
+            {id:1, txt:"This is Software Saturdays"},
+            {id:2, txt:"This array is dynamically resizable"}
+        ],
+    }
+});
